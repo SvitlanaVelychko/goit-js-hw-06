2d5ba046f57c2367bf5c55d6ad282f788484c5e7
SvitlanaVelychko <svel2608@gmail.com>
2022-05-22 09:27:40 +0300

    Solving tasks

--- js/task-01.js
+++ js/task-01.js
@@ -1,6 +1,6 @@
 const totalCategoriesEl = document.querySelectorAll('.item');
 console.log(`Number of categories: ${totalCategoriesEl.length}`);
 
-const itemEl = [...totalCategoriesEl].forEach((element) =>
-    console.log(`Category: ${element.firstElementChild.textContent}
-Elements: ${element.lastElementChild.children.length}`));
+const itemEl = totalCategoriesEl.forEach((item) =>
+    console.log(`Category: ${item.firstElementChild.textContent}
+Elements: ${item.lastElementChild.children.length}`));
